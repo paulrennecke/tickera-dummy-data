d8df826e3400057a4e74038fe644813044db8b7d
Paul Rennecke <paul.rennecke@gmail.com>
2025-01-07 18:57:24 +0100

    Update Dummy-Daten eintragen-1.0.user.js

--- Dummy-Daten eintragen-1.0.user.js
+++ Dummy-Daten eintragen-1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tickera dummy data
 // @namespace    https://ticketree.de/
-// @version      1.0
+// @version      1.1
 // @description  Add a button that fills dummy-data
 // @author       Paul Rennecke
 // @match        https://ticketree.de/kasse/
